6a0da7eaa5673ce90fa1df3f929580b4e12eabd5
Edivan de Castro <edivandecastro@gmail.com>
2020-01-09 09:16:24 -0300

    CRUD

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -12,5 +12,24 @@ module.exports = {
   async store(req, res) {
     const product = await Product.create(req.body.product);
     res.status(200).send({ "message": "product register with success", "product": product});
-  }
+  },
+
+  async show(req, res) {
+    const { id } = req.params
+    const product = await Product.findById(id);
+
+    return res.json(product);
+  },
+
+  async update(req, res) {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body.product, { new: true });
+
+    return res.json(product);
+  },
+
+  async destroy(req, res) {
+    await Product.findByIdAndRemove(req.params.id);
+
+    res.send({ "message": "product remove with success" });
+  },
 }
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,9 @@ const Product = mongoose.model('Product');
 const ProductController = require('./controllers/ProductController');
 
 routes.get('/products', ProductController.index);
+routes.get('/products/:id', ProductController.show);
 routes.post('/products', ProductController.store);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.destroy);
 
 module.exports = routes;
